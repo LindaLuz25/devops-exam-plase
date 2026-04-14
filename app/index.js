aa1cef485e0b79bf7ba2a739f25c5327909a0b84
Linda Plasencia <lindaluz25@outlook.es>
2026-04-14 12:40:25 -0500

    updated

--- app/index.js
+++ app/index.js
@@ -7,7 +7,10 @@ app.get('/', (req, res) => {
   res.send("Hello DevOps 🚀");
 });
 
+
+// Añade esto en tu index.js para que sea más claro
 app.get('/health', (req, res) => {
+  console.log("Health check received");
   res.status(200).send("OK");
 });
 
@@ -15,6 +18,7 @@ app.get('/health', (req, res) => {
 app.get('/fail', (req, res) => {
   res.status(500).send("FAIL");
 });
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
